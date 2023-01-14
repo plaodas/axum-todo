0c2a866174503eaa553fe24dad9ef10228ed25f7
Hisanaga Agake <agake@plao-das.com>
2023-01-14 20:47:57 +0900

    add validation

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,16 +1,53 @@
+use anyhow::Ok;
 use axum::{
-    extract::Extension,
+    async_trait,
+    extract::{Extension, FromRequest, Path, FromRequestParts},
     http::StatusCode,
     response::IntoResponse,
-    Json, 
+    BoxError, Json, 
 };
+use serde::de::DeserializeOwned;
+use tracing_subscriber::fmt::format;
 use std::sync::Arc;
+use validator::Validate;
 
-use crate::repositories::{CreateTodo, TodoRepository};
+use crate::repositories::{CreateTodo, TodoRepository, UpdateTodo};
+// use axum_macros::debug_handler;
 
+// #[debug_handler]
+#[derive(Debug)]
+pub struct ValidatedJson<T>(T);
+
+#[async_trait]
+impl<T,B> FromRequest<B> for ValidatedJson<T>
+where
+    T: DeserializeOwned + Validate,
+    B: http_body::Body + Send,
+    B::Data: Send,
+    B::Error: Into<BoxError>,
+{
+    type Rejection = (StatusCode, String);
+
+    async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection>{
+        let Json(value) = Json::<T>::from_request(req).await.map_err(|rejection|{
+            let message = format!("Json parse error: [{}]", rejection);
+            (StatusCode::BAD_REQUEST, message)
+        })?;
+        value.validate().map_err(|rejection|{
+            let message = format!("Validation error: [{}]", rejection).replace('\n', ", ");
+            (StatusCode::BAD_REQUEST, message)
+        });
+
+        Ok(ValidatedJson(value))
+    }
+
+
+
+}
 
 pub async fn create_todo<T: TodoRepository>(
-    Json(payload): Json<CreateTodo>, 
+    // Json(payload): Json<CreateTodo>, 
+    ValidatedJson(payload): ValidatedJson<CreateTodo>,
     Extension(repository): Extension<Arc<T>>,
 ) -> impl IntoResponse {
 
@@ -19,3 +56,50 @@ pub async fn create_todo<T: TodoRepository>(
     (StatusCode::CREATED, Json(todo))
 
 }
+
+pub async fn find_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    Extension(repository): Extension<Arc<T>>,
+) -> Result<impl IntoResponse, StatusCode >{
+
+    let todo = repository.find(id).ok_or(StatusCode::NOT_FOUND)?;
+    Ok( (StatusCode::OK, Json(todo)) )
+}
+
+pub async fn all_todo<T: TodoRepository>(
+    Extension(repository): Extension<Arc<T>>,
+) -> impl IntoResponse{
+
+    let todo = repository.all();
+    (StatusCode::OK, Json(todo))
+
+
+}
+
+pub async fn update_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    // Json(payload): Json<UpdateTodo>, 
+    ValidatedJson(payload): ValidatedJson<UpdateTodo>,
+    Extension(repository): Extension<Arc<T>>,
+) -> Result<impl IntoResponse, StatusCode> {
+
+    let todo = repository
+            .update(id, payload)
+            .or(Err(StatusCode::NOT_FOUND))?;
+
+    Ok((StatusCode::CREATED, Json(todo)))
+
+}
+
+
+pub async fn delete_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    Extension(repository): Extension<Arc<T>>,
+) -> StatusCode {
+    repository
+        .delete(id)
+        .map(|_| StatusCode::NO_CONTENT)
+        .unwrap_or(StatusCode::NOT_FOUND)
+}
+
+
